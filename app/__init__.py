7e17b62de39d7db9d017233472d61fc133c4f087
alexaaqil <alexdequantes@gmail.com>
2023-08-10 00:26:16 +0300

    first commit of the application

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from os import path
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+db = SQLAlchemy()
+DB_NAME = "bodabodainsurance.sqlite3"
+app.config['SECRET_KEY'] = 'thequickbrownfoxjumpsoverthelazydog'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), DB_NAME)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+
+from app import routes, models
+
+
+from app.models import User, Booking
+with app.app_context():
+    db.create_all()
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
---
+++ app/models.py
@@ -0,0 +1,28 @@
+from app import db
+from flask_login import UserMixin
+from datetime import datetime
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    national_id = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    package = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+class UserBooking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    booking_id = db.Column(db.Integer, db.ForeignKey('booking.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    user = db.relationship('User', backref='user_bookings')
+    booking = db.relationship('Booking', backref='user_bookings')
